0f77bb2ea4cad37bfe3e11e0e326765610c177b2
Jack Devonshire <itunes.jd@hotmail.com>
2024-02-04 16:40:15 +0000

    Now extracting class name and confidence from an image

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from ultralytics import YOLO
+import json
 
 # Load a model
 model = YOLO('yolov8m-oiv7.pt')  # pretrained YOLOv8n model
 
 # Run batched inference on a list of images
-results = model(['https://d27pcll2dx97vv.cloudfront.net/info/wp-content/uploads/2022/04/Shuiping.jpg'])  # return a list of Results objects
+results = model('https://d27pcll2dx97vv.cloudfront.net/info/wp-content/uploads/2022/04/Shuiping.jpg')  # return a list of Results objects
 
+result = json.loads(results[0].tojson())
 
-# Process results list
-for result in results:
-    boxes = result.boxes  # Boxes object for bbox outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
+for classification in result:
+    name = classification["name"]
+    confidence = float(classification["confidence"])
+
+    print(name, confidence)
